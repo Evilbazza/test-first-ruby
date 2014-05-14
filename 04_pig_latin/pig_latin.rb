c2cbd04d0a29c12442151f318c0610ce64cd0102
Evilbazza <joe.barratt@gmail.com>
2014-05-14 21:37:40 +0100

    04 ugly but done

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,16 +1,21 @@
 def translate(str)
   vowels = ["a","e","i","o","u"]
   words = str.split(' ')
-  result = ""
+  result = []
   words.each do |word|
     if vowels.include?(word[0])
-      return word + "ay"
+      result.push(word + "ay")
+	elsif word[0..1] == "qu"
+	  result.push(word[2..-1] + word[0..1] + "ay")
+	elsif word[1..2] == "qu"
+	  result.push(word[3..-1] + word[0..2] + "ay")  
+	elsif !vowels.include?(word[0]) && !vowels.include?(word[1]) && !vowels.include?(word[2])
+	  result.push(word[3..-1] + word[0..2] + "ay")  
+	elsif !vowels.include?(word[0]) && !vowels.include?(word[1])
+	  result.push(word[2..-1] + word[0..1] + "ay")  
     else
-      return word[1..-1] + word[0] + "ay"		
+      result.push(word[1..-1] + word[0] + "ay")
     end
   end  
-  result
+  result.join(' ')
 end
-
-puts "#{translate("apple")}"
-puts "#{translate("banana")}"
